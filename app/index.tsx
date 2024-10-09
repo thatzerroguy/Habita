eba5241795cfe29bc02aed782bc09c15b516ce27
Nduka Ugochukwu <ndukaugochukwu9@gmail.com>
2024-10-09 11:52:50 +0100

    Added fonts and colors

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     className="flex-1 items-center justify-center bg-Sunset"
     >
       <Text className="text-black text-3xl font-bold">Edit app/index.tsx to edit this screen.</Text>
     </View>
